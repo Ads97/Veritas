1a42cd58f4b5c5d7dbc76710866cb12903ed1541
Ads97 <advaithsridhar08@gmail.com>
2025-09-13 21:16:15 -0700

    failed facebook search attempt

--- facebook_search.py
+++ facebook_search.py
@@ -14,12 +14,13 @@ async def main():
     company_credentials = {'x_user': fb_user, 'x_pass': fb_password}
     llm = ChatOpenAI(model="gpt-4.1")
     task = "Go to this url https://www.facebook.com/groups/746424749257725/user/100066500240746 and find the user's name. Use login credentials x_user for email and x_pass for password"
-    browser = Browser(
-    executable_path='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-    user_data_dir='~/Library/Application Support/Google/Chrome',
-    profile_directory='Advaith',
-)
-    agent = Agent(task=task, llm=llm, sensitive_data=company_credentials, browser=browser)
+    company_credentials = {'x_user': os.getenv("FB_EMAIL"), 'x_pass': os.getenv("FB_PASSWORD")}
+    # browser = Browser(
+    # executable_path='/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    # user_data_dir='~/Library/Application Support/Google/Chrome',
+    # profile_directory='Advaith',
+    # )
+    agent = Agent(task=task, llm=llm, sensitive_data=company_credentials)
     await agent.run()
 
 if __name__ == "__main__":
